642f1247aa6588ac9d82f613853b71e0d06fef45
wilxim <wylopez514@gmail.com>
2013-02-11 21:37:10 -0500

    objectiveC
    creado desde xcode utilizando github

---
+++ portafolio/portafolio/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  portafolio
+//
+//  Created by Wilmer Lopez gil on 11/02/13.
+//  Copyright (c) 2013 Wilmer Lopez gil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
